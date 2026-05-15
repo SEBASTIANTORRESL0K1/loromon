29b511237164fce66db261f0e6eca1c0eabb6302
SEBASTIANTORRESL0K1 <svelasco13@ucol.mx>
2026-05-15 10:06:54 -0600

    prueba de funcionalidad del endpoint inventario

--- backend/src/routes/captura.routes.js
+++ backend/src/routes/captura.routes.js
@@ -1,10 +1,13 @@
 // src/routes/captura.routes.js
 import { Router } from 'express';
-import { capturarPersonaje } from '../controllers/captura.controller.js';
+import { capturarPersonaje, getCapturasUsuario } from '../controllers/captura.controller.js';
 
 const router = Router();
 
 // Ruta para que un usuario capture un personaje
 router.post('/capturar', capturarPersonaje);
+
+// Ruta para obtener los personajes capturados por un usuario
+router.get('/usuario/:id_usuario', getCapturasUsuario);
 
 export default router;
